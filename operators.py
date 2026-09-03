b6bf1b3778eb86ec9887b29170a5d5e0a956ee70
sunainagudla-lang <sunainagudla@gmail.com>
2026-09-03 12:58:55 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,17 @@
+#pass or fail checker 
+marks = int(input("Enter marks:"))
+
+print("passed:",marks >= 40)
+
+#login validation
+correct_username = "sunaina"
+correct_password = "2008"
+
+username =input("Enter username: ")
+password = input("Enter password: ")
+
+print(username == correct_username)
+print(password == correct_password)
+
+
+
